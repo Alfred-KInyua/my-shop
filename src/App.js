3c79923bcabd4a158ae57089d4cfce16c000ee23
Kinyua <alfkinyua@gmail.com>
2022-08-30 11:17:15 +0300

    Add Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Details from './components/pages/details/Details';
+import Home from './components/pages/home/home';
+import Nav from './components/pages/nav/Nav';
 
-export default function App() {
-  return <div>App</div>;
-}
+const App = () => (
+  <Router>
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="details" element={<Details />} />
+    </Routes>
+  </Router>
+);
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
