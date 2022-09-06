910aedeeaca70ec3ce25de2f28a39abb885c2e19
Kinyua <alfkinyua@gmail.com>
2022-09-06 16:08:23 +0300

    Add outofocus

--- src/components/AddItems.js
+++ src/components/AddItems.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { FaPlus } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 
-const AddItems = ({ newItem, setNewItem, handleSubmit }) => (
-  <form className="addForm" onSubmit={handleSubmit}>
-    <label> Add Item</label>
-    <input
-      type="text"
-      id="addItem"
-      placeholder="Add item"
-      required
-      value={newItem}
-      onChange={(e) => setNewItem(e.target.value)}
-    />
-    <button type="submit" aria-label="Add item">
-      <FaPlus />
-    </button>
-  </form>
-);
+const AddItems = ({ newItem, setNewItem, handleSubmit }) => {
+  const inputRef = useRef();
+  return (
+    <form className="addForm" onSubmit={handleSubmit}>
+      <label> Add Item</label>
+      <input
+        ref={inputRef}
+        type="text"
+        id="addItem"
+        placeholder="Add item"
+        required
+        value={newItem}
+        onChange={(e) => setNewItem(e.target.value)}
+      />
+      <button
+        type="submit"
+        aria-label="Add item"
+        onClick={() => inputRef.current.focus()}
+      >
+        <FaPlus />
+      </button>
+    </form>
+  );
+};
 
 AddItems.propTypes = {
   newItem: PropTypes.shape.isRequired,
